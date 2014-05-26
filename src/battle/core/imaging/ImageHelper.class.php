44ca0bfc79bf22580e2a3fac8180c0fed98c7d7a
JonPotiron <jon.potiron@gmail.com>
2014-05-26 18:55:00 +0200

    Core: added hex_to_rgb in image.helper

--- src/battle/core/imaging/ImageHelper.class.php
+++ src/battle/core/imaging/ImageHelper.class.php
@@ -185,5 +185,19 @@ class ImageHelper{
 		imagedestroy($thumbnail_gd_image);
 		return true;
 	}
+	
+	public static function hex_to_rgb($hex){
+		$hex = str_replace('#','',$hex);
+		if(strlen($hex) == 3) {
+			$r = hexdec(substr($hex,0,1).substr($hex,0,1));
+			$g = hexdec(substr($hex,1,1).substr($hex,1,1));
+			$b = hexdec(substr($hex,2,1).substr($hex,2,1));
+		} else {
+			$r = hexdec(substr($hex,0,2));
+			$g = hexdec(substr($hex,2,2));
+			$b = hexdec(substr($hex,4,2));
+		}
+		return array($r,$g,$b);
+	}
 }
 ?>
