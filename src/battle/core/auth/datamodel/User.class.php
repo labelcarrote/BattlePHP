c8830ce8eb52143fa5034f6e2bbdd96455231f04
touchypunchy <touchypunchy@gmail.com>
2014-07-30 22:48:34 +0200

    Core : cleaned up User model class

--- src/battle/core/auth/datamodel/User.class.php
+++ src/battle/core/auth/datamodel/User.class.php
@@ -29,10 +29,6 @@ class User extends ValueObject{
 				$this->$field = $value; //note : $this->$field ! with a $ before the field!
 		}
 	}
-
-	public function __toString(){
-		return "user [ $id $role_id $role_name $application $mail $login $hashed_password $has_confirmed $date_creation $last_ip $mark_for_deletion $mark_for_deletion_date $confirmation_token ]";
-	}
 	
 	public static function create_user_from_db($userdb){
 		return new User($userdb);
@@ -43,25 +39,6 @@ class User extends ValueObject{
 		foreach($usersdb as $userdb)
 			$users[] = self::create_user_from_db($userdb);
 		return $users;
-	}
-
-	// WIP
-	// TODO : from which post ? any form submit concerning a user? which one ? 
-	public static function create_user_from_post(){
-		$user = new User();
-		$user->id = Request::isset_or($_POST['id'],0);
-		$user->role_id = Request::isset_or($_POST['role_id'], 1);
-		$user->mail = Request::isset_or($_POST['mail'], "");
-		$user->application = Request::isset_or($_POST['application'], "flipapart");
-		$user->login = Request::isset_or($_POST['login'], "");
-		$user->hashed_password = Request::isset_or($_POST['hashed_password'], "");
-		$user->has_confirmed = Request::isset_or($_POST['has_confirmed'], false);
-		$user->date_creation = Request::isset_or($_POST['date_creation'], "");
-		$user->last_ip = Request::isset_or($_POST['last_ip'], "");
-		$user->marked_for_deletion = Request::isset_or($_POST['marked_for_deletion'], false); 
-		$user->marked_for_deletion_date = Request::isset_or($_POST['marked_for_deletion_date'], "");
-		$user->confirmation_token = Request::isset_or($_POST['confirmation_token'], "");
-		return $user;
 	}
 }
 ?>
