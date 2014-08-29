83f01f466af9005c2006f4aa0397663ab56fe229
touchypunchy <touchypunchy@gmail.com>
2014-08-29 14:46:32 +0200

    Core : updated UserManager

--- src/battle/core/auth/UserManager.class.php
+++ src/battle/core/auth/UserManager.class.php
@@ -4,6 +4,31 @@ require_once 'core/auth/db/UserDB.class.php';
  * UserManager
  */
 class UserManager{
+	// -----------------
+	// ---- QUERIES ----
+	// -----------------
+
+	public static function get_user($user_id){
+		return User::create_user_from_db(UserDB::getInstance()->get_user($user_id));
+	}
+
+	public static function get_user_from_confirmation_token($confirmation_token,$application = null){
+		if($application === null)
+			$application = Request::get_application();
+
+		return User::create_user_from_db(UserDB::getInstance()->get_user_from_confirmation_token($confirmation_token,$application));
+	}
+
+	public static function get_user_from_login($login,$application = null){
+		if($application === null)
+			$application = Request::get_application();
+
+		return User::create_user_from_db(UserDB::getInstance()->get_user_from_login($login,$application));
+	}
+
+	// -------------------------------------------
+	// ---- COMMANDS : Create, Update, Delete ----
+	// -------------------------------------------
 
 	public static function save($user){
 		if($user === null)
