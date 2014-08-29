2fdcc8aee34a1d82061f153136b1e05ede3260b5
touchypunchy <touchypunchy@gmail.com>
2014-08-29 16:04:47 +0200

    Core : updated UserManager

--- src/battle/core/auth/UserManager.class.php
+++ src/battle/core/auth/UserManager.class.php
@@ -8,8 +8,10 @@ class UserManager{
 	// ---- QUERIES ----
 	// -----------------
 
-	public static function get_user($user_id){
-		return User::create_user_from_db(UserDB::getInstance()->get_user($user_id));
+	public static function get_user($user_id,$application = null){
+		if($application === null)
+			$application = Request::get_application();
+		return User::create_user_from_db(UserDB::getInstance()->get_user($user_id,$application));
 	}
 
 	public static function get_user_from_confirmation_token($confirmation_token,$application = null){
@@ -24,6 +26,13 @@ class UserManager{
 			$application = Request::get_application();
 
 		return User::create_user_from_db(UserDB::getInstance()->get_user_from_login($login,$application));
+	}
+
+	public static function get_users($page_id, $nb_user_by_page,$application = null){
+		if($application === null)
+			$application = Request::get_application();
+
+		return User::create_users_from_db(UserDB::getInstance()->get_users(1,100,$application));
 	}
 
 	// -------------------------------------------
